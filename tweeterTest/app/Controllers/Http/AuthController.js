3202a6ea230e8edd7b0be00eb315c6deda686e73
gusttavo212 <gusttavo212@gmail.com>
2019-02-11 10:42:56 -0200

    User register and authenticate

--- tweeterTest/app/Controllers/Http/AuthController.js
+++ tweeterTest/app/Controllers/Http/AuthController.js
@@ -1,7 +1,24 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class AuthController {
-  async register()
+  async register({ request }) {
+    //usando desestruturação para pegar cada dado
+    const data = request.only(['email', 'password', 'username'])//Request manda isso
+
+    const user = await User.create(data)
+
+    return user
+  }
+
+  async authenticate({ request, auth }) {
+    const { email, password } = request.all()
+
+    const token = await auth.attempt(email, password)//Requisitar um toke JWT
+
+    return token;
+  }
 }
 
 module.exports = AuthController
